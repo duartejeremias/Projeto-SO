1f9f8875493b2d12548c9f67d1d38ee1cd04afce
duartejeremias <dmjeremias@gmail.com>
2020-10-23 18:08:41 +0100

    Added ex02

---
+++ ex02/README.md
@@ -0,0 +1,89 @@
+# SO Project 2020-21
+
+## Exercise 2 base code.
+
+## Pseudocodigo/alteracoes a fazer
+
+### 1.Sincronizacao fina dos inodes
+
+* O desempenho paralelo do exercicio 1 foi desapontante devido ao metodo de sincronizacao utilizado;
+
+* Temos de implementar uma nova estrategia de sincronizacao que permita maior paralelismo completamente diferente da do primeiro exercicio, portanto temos de alterar o codigo de modo a que nao seja recebido o 4o argumento de linha de comandos (__synchstrategy__).
+
+* Em vez de trinco global, agora devemos colocar um trinco por inode (temos de decidir entre rwlock ou mutex em cada caso);
+
+* Basicamente, ao percorrer path de cada diretoria/ficheiro (cadeia de inodes) para executar os comandos, devemos fazer lock de acordo com os requisitos para assegurar sincronizacao;
+
+* Apos execucao dos comandos, destranca-se os locks pela ordem contraria a de trancacao (nao sei se trancacao existe mas vai);
+
+* Temos de assegurar que as operacoes nao alterem o estado do _TecnicoFS_ nem elementos comuns a este.
+
+> Exemplo 1:
+    op1: c /a/b/c f
+    op2: c /a/d/f f
+    Estas operacoes alteram o estado das diretorias /a/b e /a/d respetivamente enquanto que apenas leem (nao mudam) as diretorias / (root) e /a, logo podem e devem executar-se concorrentemente. Se a op1 fosse: c/a/c f, ja nao seria possivel correr concorrentemente a op2
+> Exemplo 2:
+    op1: l /
+    op2: c /a d
+    Estas operacoes nao podem ser executadas concorrentemente pois a op2 esta a criar uma diretoria (inode) no mesmo path da op1 (/ ou root)
+
+### 2.Execucao incremental de comandos
+
+* Devemos alterar o metodo de carregamento de comandos para o programa;
+
+* Devera existir uma tarefa (produtora) encarregue de carregar o vetor (buffer) de comandos ao mesmo tempo que as outras tarefas (consumidoras), definidas pelo __numthreads__ do terminal, vao executando-as;
+
+* Assim que uma tarefa esteja livre devera ir buscar o proximo comando do buffer;
+
+* Se o buffer ficar cheio de comandos, a tarefa encarregue de os carregar devera esperar;
+
+* Se uma tarefa consumidora for buscar um comando para executar e o buffer estiver vazio, devera esperar ate ao proximo comando for carregado;
+
+* O buffer de comandos devera passar a ter 10 entradas e deve ser tratado como um vetor circular;
+
+* A tarefa produtora devera carregar ficheiros ate ao EOF ser alcancado;
+
+* Devemos alterar o tempo de execucao, este devera comecar antes do carregamento de comandos comecar/.
+
+### 3.Nova op: mover ficheiro ou diretoria
+
+* Devemos implementar uma nova op (m);
+
+* Esta op recebe dois argumentos: o pathname atual de uma diretoria/ficheiro e o novo pathname;
+
+* Esta op retira a entrada (__dirEntry__) com o pathname atual e insere uma nova entrada com o novo pathname;
+
+* O inumber na nova entrada deve manter-se igual ao da entrada original;
+
+* Tem de ser verificadas duas condicoes no momento da invocacao:
+  * tem existir um ficheiro/diretoria com o pathname atual;
+  * nao pode existir ficheiro/diretoria com o novo pathname;
+
+* Caso uma ou mais das condicoes nao se verifiquem, a op deve ser cancelada, nao tendo efeitos em relacao as outras tarefas que possam estar a ser executadas concorrentemente;
+
+* Devemos prevenir situacoes de interblocagem ou mingua.
+
+> Exemplo:
+    op1: m /dA/f1 /dB/f2
+    Esta op nao pode ser executada se nao existir o f1 na diretoria A;
+    Esta op nao pode ser executada se o f2 da diretoria B ja existir;
+    Caso contrario pode-se executar.
+
+### 4.Nova op: Shell script
+
+* Devemos criar um shell script chamado __runTests.sh__;
+
+* Este script devera avaliar o desempenho do TecnicoFS quando executado com diferentes argumentos e ficheiros de entrada;
+
+* O script devera receber os seguintes tres argumentos:
+  * inputdir;
+  * outputdir;
+  * maxthreads;
+
+* O script devera usar os diferentes ficheiros de teste contidos em __inputdir__ com um numero random de threads entre 1 e __maxthreads__;
+
+* Antes de cada teste, o script devera imprimir: "InputFile=__nomeDoFicheiro__ NumThreads=__numeroDeTarefas__";
+
+* Apos cada teste, o script devera apenas imprimir: "TecnicoFS completed in __duracao__ seconds";
+
+* Apos cada teste, devera ser criado na __outputdir__ um ficheiro contendo o estado final do FS. O nome do ficheiro devera ser "__nomeFicheiroEntrada__-__numeroDeTarefas__.txt"
---
+++ ex02/main.c
@@ -0,0 +1,393 @@
+/* 
+ * Primeiro exercicio do projeto de SO
+ * Made by:
+ * Duarte Jeremias, 96857
+ * Rodrigo Liu, 96909
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <ctype.h>
+#include "fs/operations.h"
+
+#define MAX_COMMANDS 150000
+#define MAX_INPUT_SIZE 100
+#define TRUE 1
+#define FALSE 0
+#define MUTEX 1
+#define RWLOCK 2
+#define NOSYNC 3
+#define CMD 4
+#define RD 5
+#define WR 6
+
+int numberThreads = 0;
+
+int lockType = 0;
+
+pthread_mutex_t mutex;
+pthread_mutex_t commandLock;
+pthread_rwlock_t rwlock;
+
+char inputCommands[MAX_COMMANDS][MAX_INPUT_SIZE];
+int numberCommands = 0;
+int headQueue = 0;
+
+int insertCommand(char* data) {
+    if(numberCommands != MAX_COMMANDS) {
+        strcpy(inputCommands[numberCommands++], data);
+        return 1;
+    }
+    return 0;
+}
+
+char* removeCommand() {
+    if(numberCommands > 0){
+        numberCommands--;
+        return inputCommands[headQueue++];
+    }
+    return NULL;
+}
+
+void errorParse(){
+    fprintf(stderr, "Error: command invalid\n");
+    exit(EXIT_FAILURE);
+}
+
+void processInput(char *inputFileName){
+    char line[MAX_INPUT_SIZE];
+    FILE *inputFile = fopen(inputFileName, "r");
+
+    if(inputFile == NULL){
+      fprintf(stderr, "Error: file %s does not exist\n", inputFileName);
+      return;
+    }
+
+    /* break loop with ^Z or ^D */
+    while (fgets(line, sizeof(line)/sizeof(char), inputFile)) {
+        char token, type;
+        char name[MAX_INPUT_SIZE];
+
+        int numTokens = sscanf(line, "%c %s %c", &token, name, &type);
+
+        /* perform minimal validation */
+        if (numTokens < 1) {
+            continue;
+        }
+        switch (token) {
+            case 'c':
+                if(numTokens != 3)
+                    errorParse();
+                if(insertCommand(line))
+                    break;
+                return;
+
+            case 'l':
+                if(numTokens != 2)
+                    errorParse();
+                if(insertCommand(line))
+                    break;
+                return;
+
+            case 'd':
+                if(numTokens != 2)
+                    errorParse();
+                if(insertCommand(line))
+                    break;
+                return;
+
+            case '#':
+                break;
+
+            default: { /* error */
+                errorParse();
+            }
+        }
+    }
+    fclose(inputFile);
+}
+
+/*
+ * Given a mode, this function locks the thread
+ * Input: lock mode
+ */
+void lock(int mode){
+   //if there isnt syncronization
+   if(lockType == NOSYNC) return;
+
+   //if lock necessary is the command lock
+   else if(mode == CMD){
+      if(pthread_mutex_lock(&commandLock)){
+         fprintf(stderr, "Error: Mutex failed to lock\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+
+   //if locktype is mutex
+   else if(lockType == MUTEX){
+      if(pthread_mutex_lock(&mutex)){
+         fprintf(stderr, "Error: Mutex failed to lock\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+
+   //if locktype is rwlock
+   else if(lockType == RWLOCK){
+      //if rwlock necessary is RD
+      if(mode == RD){
+         if(pthread_rwlock_rdlock(&rwlock)){
+            fprintf(stderr, "Error: Rwlock failed to lock\n");
+            exit(EXIT_FAILURE);
+         }
+      }
+      //if rwlock necessary is WR
+      if(mode == WR){
+         if(pthread_rwlock_wrlock(&rwlock)){
+            fprintf(stderr, "Error: Rwlock failed to lock\n");
+            exit(EXIT_FAILURE);
+         }
+      }
+   }
+}
+
+/*
+ * Given a mode, this function unlocks the locked thread set earlier
+ * Input: lock mode
+ */
+void unlock(int mode){
+   //if there isnt syncronization
+   if(lockType == NOSYNC) return; 
+
+   //if lock is for the commands
+   if(mode == CMD){ 
+      if(pthread_mutex_unlock(&commandLock)){
+         fprintf(stderr, "Error: Mutex failed to unlock\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+   //if lock is mutex
+   else if(lockType == MUTEX){
+      if(pthread_mutex_unlock(&mutex)){
+         fprintf(stderr, "Error: Mutex failed to unlock\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+   //if lock is rwlock
+   else if(lockType == RWLOCK){
+      if(pthread_rwlock_unlock(&rwlock)){
+         fprintf(stderr, "Error: rwlock failed to unlock\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+}
+
+void *applyCommands(void*ptr){
+   while (TRUE){
+
+      lock(CMD);
+      if(numberCommands == 0){ //if command count is 0, thread should end
+         unlock(CMD);
+         break;
+      }
+      unlock(CMD);
+
+      lock(CMD);
+      const char* command = removeCommand();
+
+      if (command == NULL){ //if command is NULL, thread should end
+         unlock(CMD);
+         break;
+      }
+      unlock(CMD);
+
+
+      char token, type;
+      char name[MAX_INPUT_SIZE];
+      int numTokens = sscanf(command, "%c %s %c", &token, name, &type);
+      if (numTokens < 2) {
+         fprintf(stderr, "Error: invalid command in Queue\n");
+         exit(EXIT_FAILURE);
+      }
+
+      int searchResult;
+      switch (token) {
+         case 'c':
+            switch (type) {
+               case 'f':
+                  fprintf(stdout, "Create file: %s\n", name);
+                  lock(WR); 
+                  create(name, T_FILE);
+                  unlock(0);
+                  break;
+               case 'd':
+                  fprintf(stdout,"Create directory: %s\n", name);
+                  lock(WR);
+                  create(name, T_DIRECTORY);
+                  unlock(0);
+                  break;
+               default:
+                  fprintf(stderr, "Error: invalid node type\n");
+                  exit(EXIT_FAILURE);
+            }
+            break;
+
+         case 'l':
+            lock(RD);
+            searchResult = lookup(name);
+            unlock(0);
+            if (searchResult >= 0)
+               fprintf(stdout, "Search: %s found\n", name);
+            else
+               fprintf(stdout, "Search: %s not found\n", name);
+            break;
+
+         case 'd':
+            fprintf(stdout, "Delete: %s\n", name);
+            lock(WR);
+            delete(name);
+            unlock(0);
+            break;
+         default: { /* error */
+            fprintf(stderr, "Error: command to apply\n");
+            exit(EXIT_FAILURE);
+         }
+      }
+   }
+   return NULL;
+}
+
+/*
+ * Parsing of the command line arguments
+ * Input: argument count and arguments
+ */
+void argParse(int argc, char* argv[]){
+   if(argc != 5)
+      fprintf(stderr, "Error: Incorrect number of arguments given.\n");
+   else if(!argv[1])
+      fprintf(stderr, "Error: Input file given is NULL.\n");
+   else if(!argv[2])
+      fprintf(stderr, "Error: Output file given is NULL.\n");
+   else if(atoi(argv[3]) <= 0)
+      fprintf(stderr, "Error: Invalid number of threads given.\n");
+   else if(!argv[4])
+      fprintf(stderr, "Error: Synch method is NULL.\n");
+   else {
+      numberThreads = atoi(argv[3]);  //sets global var for the number of threads
+
+      if(!strcmp(argv[4], "mutex")) lockType = MUTEX;  //if sync type is mutex
+
+      else if(!strcmp(argv[4], "rwlock")) lockType = RWLOCK;  //if sync type is rwlock
+
+      else if(!strcmp(argv[4], "nosync")){  //if sync type is nosync, there cant be more than 1 thread
+         if(numberThreads > 1){ 
+            fprintf(stderr, "Error: NoSync only available with 1 thread execution.\n");
+            exit(EXIT_FAILURE);
+         }
+         lockType = NOSYNC;
+      }
+      else {
+         fprintf(stderr, "Error: Invalid sync method.\n");
+         exit(EXIT_FAILURE);
+      }
+      return;
+   }
+   exit(EXIT_FAILURE);
+}
+
+void init_lock(){
+   if(lockType == NOSYNC) return; //if locktype is nosync, no need to initialize lock
+
+   //if locktype is either mutex or rwlock, afterwards another mutex is initialized to acess the inputCommands vector
+   if(lockType == MUTEX){ 
+      if(pthread_mutex_init(&mutex, NULL)){
+         fprintf(stderr, "Error: Mutex failed to init\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+   else if(lockType == RWLOCK){
+      if(pthread_rwlock_init(&rwlock, NULL)){
+         fprintf(stderr, "Error: rwlock failed to init\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+   if(pthread_mutex_init(&commandLock, NULL)){
+      fprintf(stderr, "Error: Mutex failed to init\n");
+      exit(EXIT_FAILURE);
+   }
+}
+
+void destroy_lock(){
+   if(lockType == NOSYNC) return; //if locktype is nosync, no need to destroy the lock
+
+   if(lockType == MUTEX){
+      if(pthread_mutex_destroy(&mutex)){
+         fprintf(stderr, "Error: Failed destruction of mutex.\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+   else if(lockType == RWLOCK){
+      if(pthread_rwlock_destroy(&rwlock)){
+         fprintf(stderr, "Error: Failed destruction of rwlock.\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+   if(pthread_mutex_destroy(&commandLock)){
+      fprintf(stderr, "Error: Failed destruction of mutex.\n");
+      exit(EXIT_FAILURE);
+   }
+}
+
+int main(int argc, char* argv[]) {
+   argParse(argc, argv);
+
+   struct timeval startTime, endTime;
+   int i;
+
+   pthread_t threadPool[numberThreads];
+   gettimeofday(&startTime, NULL);
+
+   /* init filesystem */
+   init_fs();
+
+   init_lock();
+
+   /* process input and print tree */
+   processInput(argv[1]);
+
+   if(lockType == NOSYNC){
+      applyCommands(NULL);
+   } else {
+      for(i = 0; i < numberThreads; i++){
+         if(pthread_create(&threadPool[i], NULL, applyCommands, NULL)){
+            fprintf(stderr, "Error: thread creation failed.\n");
+            exit(EXIT_FAILURE);
+         }
+      }
+      for(i = 0; i < numberThreads; i++){
+         if(pthread_join(threadPool[i], NULL)){
+            fprintf(stderr, "Error: thread merging failed.\n");
+            exit(EXIT_FAILURE);
+         }
+      }
+   }
+
+   print_tecnicofs_tree(argv[2]);
+
+   gettimeofday(&endTime, NULL);
+
+   destroy_lock();
+
+
+   /* release allocated memory */
+   destroy_fs();
+
+   printf("TecnicoFS completed in %.4f seconds.\n", (endTime.tv_sec - startTime.tv_sec) + (endTime.tv_usec - startTime.tv_usec) / 1e6);
+
+   exit(EXIT_SUCCESS);
+}
